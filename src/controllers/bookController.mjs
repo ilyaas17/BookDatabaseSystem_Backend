f101c812347ee050af745ec108e9355dca72a9a3
Utkarsh Mishra <ilyaasmis17@gmail.com>
2024-06-06 16:35:56 +0530

    error-handling set up for all CRUD ops

--- src/controllers/bookController.mjs
+++ src/controllers/bookController.mjs
@@ -46,40 +46,45 @@ const findBook = async (req,res)=>{
 
 const deleteBook = async (req,res)=>{
   
-  const deleteThisID = req.params.id;
+  try{
+    const deleteThisID = req.params.id;
 
-  const deletedData = await BookDbModel.findByIdAndDelete({_id:`${deleteThisID}`})
-  .then(() => res.status(200).send({
-    status: "Success",
-    message: "Book Deleted Successfully",
-  }))
-  .catch((e)=>res.send({
-    status:404, 
-    message: "Book Not Found", 
-    error:e}))
+    const deletedData = await BookDbModel.findByIdAndDelete({_id:`${deleteThisID}`})
+    .then(() => res.status(200).send({
+      status: "Success",
+      message: "Book Deleted Successfully",
+    }));
+  
+  } catch(e) {
+      res.status(500).send({
+        status: "Failure",
+        message : e.message || "An error occurred  while deleting the book."
+      })
+  }
 }
 
 const updateBook = async (req, res)=>{
 
-  const updateThisData = req.params.id;
-  const updateWithThisData = req.body;
+  try{
+    const updateThisData = req.params.id;
+    const updateWithThisData = req.body;
 
-  const updateReturnObj = await BookDbModel.updateOne(
-    {_id: updateThisData},
-    {$set:updateWithThisData})
-    .catch(err=>{
-      return res.status(404).send({
-      status:"Failure",
-      message:"Book Not Found",
-      error:err
-    })});
-    
-    const updatedData = await BookDbModel.find({_id:`${updateThisData}`})
-    return res.send({
-      status:200,
-      message:"Book Updated Successfully",
-      updatedQuery : updatedData,
-    })
+    const updateReturnObj = await BookDbModel.updateOne(
+      {_id: updateThisData},
+      {$set:updateWithThisData})
+      
+      const updatedData = await BookDbModel.find({_id:`${updateThisData}`})
+      return res.send({
+        status:200,
+        message:"Book Updated Successfully",
+        updatedQuery : updatedData,
+      })
+    } catch(e) {
+        res.status(500).send({
+          status:"Failure",
+          message: e.message || "An error occurred while processing the request."
+        })
+    }
 }
 
 export { createBook, findBook, deleteBook, updateBook };
